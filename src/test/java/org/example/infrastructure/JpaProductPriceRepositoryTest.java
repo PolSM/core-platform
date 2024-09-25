97856445ea4f9631875d786a6fb2006effd502a9
Pol <polsm95@gmail.com>
2024-09-25 18:56:16 +0200

    test: Add new test edge cases for findProductPriceByDateAndId

--- src/test/java/org/example/infrastructure/JpaProductPriceRepositoryTest.java
+++ src/test/java/org/example/infrastructure/JpaProductPriceRepositoryTest.java
@@ -47,4 +47,51 @@ public class JpaProductPriceRepositoryTest {
         );
         Assertions.assertEquals(expectedPrice, price);
     }
+
+    @Test
+    public void should_not_retrieve_a_price_if_date_before_start_date() {
+        ProductPrice newPrice = new ProductPrice(
+                35456,
+                1,
+                LocalDateTime.of(2021, 1, 1, 0, 0),
+                LocalDateTime.of(2021, 12, 31, 23, 59),
+                2,
+                1,
+                45.50f,
+                "USD"
+        );
+        productPriceRepository.save(newPrice);
+
+        ProductPrice price = productPriceRepository.findProductPriceByDateAndId(
+                LocalDateTime.of(2020, 1, 1, 0, 0),
+                35456,
+                1
+        );
+
+        Assertions.assertNull(price);
+    }
+
+    @Test
+    public void should_not_retrieve_a_price_if_date_after_start_date(){
+        ProductPrice newPrice = new ProductPrice(
+                35456,
+                1,
+                LocalDateTime.of(2021, 1, 1, 0, 0),
+                LocalDateTime.of(2021, 12, 31, 23, 59),
+                2,
+                1,
+                45.50f,
+                "USD"
+        );
+        productPriceRepository.save(newPrice);
+
+        ProductPrice price = productPriceRepository.findProductPriceByDateAndId(
+                LocalDateTime.of(2022, 1, 1, 0, 0),
+                35456,
+                1
+        );
+
+        Assertions.assertNull(price);
+    }
+
 }
